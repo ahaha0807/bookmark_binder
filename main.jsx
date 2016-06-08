41954f392f5c9b4c4d7d01cdfcc474a3d32e4c1c
ahaha0807 <b5331@oic.jp>
2016-06-08 22:10:32 +0900

    コンポーネントの定義・描画関数の定義追加
    データ登録・描画関数の作成(data_registor)
    日付登録関数の更新(set_date)
    コンテンツリスト・コンテンツ・タイトル・リンク・登録時刻・シリアルナンバー用コンポーネント定義(ContentsList ,Content ,ContentTitle ,ContentLink ,ContentTime ,ContentNumber)

--- main.jsx
+++ main.jsx
@@ -1,117 +1,103 @@
-  /*
- *  input_link    登録時に入力されたページのURI
- *  input_title   登録時に入力されたページのタイトル
- *  last_number   ページ内に表示されている一番下のリンクについたシリアルナンバー
+/*
+ *  content['title']  タイトルを保持する
+ *  content['link']   リンクを保持する
+ *  content['id']     idを保持する
+ *  content['time']   登録日時を保持する
+ *  cnt               id用のカウンタ
+ *  contentsList[]    コンテンツオブジェクト保持用配列
  *
  */
-var input_link = "http://qiita.com/ahaha0807_alg";
-var input_title = "Qiitaサイト";
-var last_number = 0;
+var contentsList = [];
+var cnt = 0;
 
-// == JS関数定義部 ==
+// === ＪＳ関数定義 ===
 
+var data_registor = () => {  //データの登録・描画
+  var content = {
+    link  : "",
+    title : "",
+    id    : 0,
+    time  : ""
+  };
+  content['title'] = document.querySelector('#data1').value;
+  content['link']  = document.querySelector('#data2').value;
+  content['id']    = cnt++;
 
-function set_date() { //日付・時刻の取得
+  contentsList.push(content);
+
+  ReactDOM.render(
+    <Contents contentsList={contentsList} />,
+    document.querySelector('#container')
+  );
+}
+
+const set_date = () => { //日付・時刻の取得
   var date = new Date();
   var year = date.getFullYear();
   var month = date.getMonth() + 1;
   var day = date.getDate();
   var hour = date.getHours();
   var minute = date.getMinutes();
-
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
   date = year + "/" + month + "/" + day + " ";
   date += hour + ":" + minute;
 
   return date;
 }
 
-//== Reactコンポーネント定義部 ==
 
-// コンテンツボックスコンポーネント定義
-var Content = React.createClass({
-  render: function() {
-    return(
-      <div className="content">
-        <ContentLink />
-        <br />
-        <ContentTitle />
-        <br />
-        <ContentTime />
-        <br />
-        <ContentSelealNumber />
-      </div>
-    );
-  }
-});
+// === Reactコンポーネント定義 ===
+const ContentsList = props => {     //コンテンツリストコンポーネント定義
+  return(
+    <ul className="contents">
+      {props.contentsList.map((content, index)=> <Content content={content} index={index} />)}
+    </ul>
+  )
+}
 
-// リンクコンポーネント定義
-var ContentLink = React.createClass({
-  render: function() {
-    return (
-      <h3 id={"link_" + last_number} className="links">
-        <a href={input_link}>{input_link}</a>
-      </h3>
-    );
-  }
-});
-
-// タイトルコンポーネント定義
-var ContentTitle = React.createClass({
-  render: function() {
-    return (
-      <h1 id={"title_" + last_number} className="titles">
-        <a href={input_link}>{input_title}</a>
-      </h1>
-    );
-  }
-});
-
-// 登録日時コンポーネント定義
-var ContentTime = React.createClass({
-  render: function() {
-    return (
-        <span id={"time_" + last_number} className="submint_time">{
-          set_date()
-        }</span>
-    );
-  }
-});
-
-// シリアルナンバーコンポーネント定義
-var ContentSelealNumber = React.createClass({
-  render: function() {
-    return (
-      <span id={"num_" + last_number} className="seleal_number">{last_number}</span>
-    );
-  }
-});
-
-
-// サンプルコンポーネント定義
-var SampleContent = React.createClass({
-  render: function() {
-    return (
-      <div id="sample_content">
-        <h3 id="sample_links" className="link"><a href="#">http://sample.com</a></h3>
-        <br />
-        <h1 id="sample_title" className="title"><a href="#">サンプルレコード</a></h1>
-        <br />
-        <span id="sample_time" className="submint_time">2000/01/01 0:00:00</span>
-        <span id="sample_num" className="seleal_number">0</span>
-      </div>
-    )
-  }
-});
-
-// レンダーメソッド実行
-if (1 == 1){  //コンポーネントがひとつもなかった場合は、サンプルコンテンツを表示する。
-  React.render(
-    <Content />,
-    document.querySelector('#contents')
+const Content = props => {      //コンテンツコンポーネント定義
+  var content = props.content;
+  return (
+    <li id={"content_" + (props.index)} className="content">
+      <ContentLink link={content.link} index={props.index} />
+      <br />
+      <ContentTitle title={content.title} index={props.index} />
+      <ContentTime time={set_date()} index={props.index} />
+      <ContentNumber index={props.index} />
+    </li>
   );
-} else {
-  React.render(
-    <SampleContent />,
-    document.querySelector('#contents')
+}
+
+const ContentTitle = props => {   //タイトルコンポーネント定義
+  return (
+    <h1 id={"title_" + props.index}>
+      {props.title}
+    </h1>
   );
+}
+
+const ContentLink = props => {    //リンクコンポーネント定義
+  return (
+    <h2 id={"link_" + props.index} className="link">
+      <a href={props.link}>{props.link}</a>
+    </h2>
+  )
+}
+
+const ContentTime = props => {   //登録時刻コンポーネント定義
+  return (
+    <span id={"time_" + props.index} className="time">
+      {props.time}
+    </span>
+  )
+}
+
+const ContentNumber = props => {  //シリアルナンバーコンポーネント定義
+  return (
+    <span id={"number_" + props.index} className="number">
+      {" " + props.index}
+    </span>
+  )
 }
